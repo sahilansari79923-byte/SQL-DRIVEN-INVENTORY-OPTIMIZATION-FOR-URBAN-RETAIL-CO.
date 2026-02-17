6fef019a51ea125493bf46d8cc1426e0b2a99dab
Md Sahil Ansari <sahilansari79923@gmail.com>
2026-02-17 23:26:35 +0530

    Update Stock Availability.sql

--- SQL SCRIPTS/Stock Availability.sql
+++ SQL SCRIPTS/Stock Availability.sql
@@ -1,5 +1,4 @@
 -- STOCK AVAILABILITY
--- Stock availability is calculated by selecting the most recent inventory snapshot for each product,rather than summing historical inventory records)
 select i.Product_ID,i.inventory_level,latest.latest_date from inventory i join(
 select Product_ID,
 max(date) as latest_date
